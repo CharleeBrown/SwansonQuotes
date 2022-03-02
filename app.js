ac62588bfe8897236222b90de37145cf45f228a4
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-03-02 10:06:58 -0600

    removed unused routing.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
